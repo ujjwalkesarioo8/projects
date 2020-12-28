2f583591fe8452c8f98ee4173a57a838ac8094c0
ujjwalkesari008 <ujjwalkesari007@gmail.com>
2020-12-28 21:19:44 +0530

    5

---
+++ mailing/data.php
@@ -0,0 +1,28 @@
+<?php
+
+ // function to connect to database
+ function doDB() {
+ global $mysqli;
+
+ //connect to server and select database
+ $mysqli = mysqli_connect("localhost", "root",
+ "", "mail");
+
+ //if connection fails, stop script execution
+ if (mysqli_connect_errno()) {
+ printf("Connect failed: %s\n", mysqli_connect_error());
+ exit();
+ }
+ }
+ // function to check email address
+ function emailChecker($email) {
+ global $mysqli, $safe_email, $check_res;
+
+ //check that email is not already in list
+ $safe_email = mysqli_real_escape_string($mysqli, $email);
+ $check_sql = "select `id` from subscribers
+ where email = '".$safe_email."'";
+ $check_res = mysqli_query($mysqli, $check_sql)
+ or die(mysqli_error($mysqli));
+ }
+?>
---
+++ mailing/mail.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 27, 2020 at 08:05 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mail`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+
+CREATE TABLE `subscribers` (
+  `id` int(5) NOT NULL,
+  `email` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subscribers`
+--
+
+INSERT INTO `subscribers` (`id`, `email`) VALUES
+(1, 'ujjwalkesari007@gmail.com'),
+(2, 'ujjwalkesari007@outlook.com'),
+(3, 'ujjwalkesari008@outlook.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
