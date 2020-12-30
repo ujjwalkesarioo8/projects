7cb398863a760e19d2e79bd1321cd915d710fd61
ujjwalkesari008 <ujjwalkesari007@gmail.com>
2020-12-30 13:23:19 +0530

    tree.sql

---
+++ tree.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2020 at 08:45 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tree`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getchild` (IN `uid` INT(5), OUT `result` VARCHAR(200))  NO SQL
+BEGIN
+
+select title into result from machine where `parent_id`=uid;
+
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `machine`
+--
+
+CREATE TABLE `machine` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `parent_id` int(20) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `machine`
+--
+
+INSERT INTO `machine` (`id`, `title`, `parent_id`) VALUES
+(1, 'Electronics', NULL),
+(2, 'Laptops & PC', 1),
+(3, 'Laptops', 2),
+(4, 'PC', 2),
+(5, 'Cameras & photo', 1),
+(6, 'Camera', 5),
+(7, 'Phones & Accessories', 1),
+(8, 'Smartphones', 7),
+(9, 'Android', 8),
+(10, 'iOS', 8),
+(11, 'Other Smartphones', 8),
+(12, 'Batteries', 7),
+(13, 'Headsets', 7),
+(14, 'Screen Protectors', 7);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `machine`
+--
+ALTER TABLE `machine`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `parent_id` (`parent_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `machine`
+--
+ALTER TABLE `machine`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `machine`
+--
+ALTER TABLE `machine`
+  ADD CONSTRAINT `machine_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `machine` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
